c19369b738e88a418e54b5793a87ea67d197532c
brightonyoung <brighton.young@outlook.com>
2020-11-16 00:58:59 -0600

    third commit

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
-# JS211_Dodgeball
+Project Scope: 20pts - Code Plan - Include this in a README.md file in your folder with comment in your code. 20pts - Can add People to Players - When clicked the people are added to the Players column and removed from the People list while getting new values of a player added to them. 20pts - Can add Players to different Teams - When we click on the blue button the Player is added to the blue team and removed from the Player list while also getting the keys color and mascot extended to them when they are moved to a team. 20pts - Uses Class - This is not a hack job. You should use class to add the new properties you need and extend when you need. 20pts - Minimum 3 Unit Tests - Use Mocha and Chai to give us at least 3 unit test that prove a person becomes a player and player becomes a teammate.
 
-JS211_DodgeBall
-Steps:
+Code Plan:
 
-Copy code from Class Textbook.
-Have players on List of People.
-Create a template of dodgeBallPlayer adding canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience. Make sure to include name and id.
-Format button "Make Player" so onClick it moves player to Dodgeball players list and adds new value of "player" while removing from previous list.
-Define redTeammate and blueTeammate constructors with id, name, team color and mascot.
-Format button for red/blue team so user can click, adding player to either team and removing them from listOfPlayers array.
-Each teammate has mascot and color added to their name for their specific team.
-Ensure that players cannot be repeated.
-Players MUST BE REMOVED from previous lists.
-Include unit tests (minimum of 3.)
+Make a class for a player that can use the information from the arrayOfPeople.
+Make an input form on the Dom that asks the necassary questions to input the registration information when moving a player from the arrayofPeople to the new player class.
+Create an area to show all registered player, with a button that allows them to pick the team they are to be placed on.
+When the button for "pick a team" is clicked, have a form that allows them to pick the team they want. when the team is picked assign them to either the red team array or blue team. Also give them keys with team name and mascot at this time in their array.
+Create a button for each player in three registered player areas (Resitstered Players, Red Team, or Blue team) that when pushed shows all the players related info.
+Create a button in the "List of Peoples area" that ties to a form to allow new people to be entered in the system.
+Make players that are not healthy show up in red.
+Add mascot and team colors displayed in the list area for the team.
+Use grid layout to create two columns of data on the DOM. Have List of people, blue team, and red team in one column. Have registered player and player info int he other column.
--- main.js
+++ main.js
@@ -1,281 +1,169 @@
-// 'use strict';
-// const assert = require('assert');
-
+'use strict';
+const assert = require('assert');
 const arrOfPeople = [
     {
-      id: 1,
-      name: "Charles Young",
-      age: 55,
-      skillSet: "welding",
-      placeBorn: "Omaha, Nebraska"
+        id: 2,
+        name: "Charles Young",
+        age: 55,
+        skillSet: "welding",
+        placeBorn: "Omaha, Nebraska"
     },
     {
-      id: 2,
-      name: "Judy Twilight",
-      age: 35,
-      skillSet: "fishing",
-      placeBorn: "Louisville, Kentucky"
+        id: 3,
+        name: "Judy Twilight",
+        age: 35,
+        skillSet: "fishing",
+        placeBorn: "Louisville, Kentucky"
     },
     {
-      id: 3,
-      name: "Cynthia Doolittle",
-      age: 20,
-      skillSet: "tic tac toe",
-      placeBorn: "Pawnee, Texas"
+        id: 4,
+        name: "Cynthia Doolittle",
+        age: 20,
+        skillSet: "tic tac toe",
+        placeBorn: "Pawnee, Texas"
     },
     {
-      id: 4,
-      name: "John Willouby",
-      age: 28,
-      skillSet: "pipe fitting",
-      placeBorn: "New York, New York"
+        id: 5,
+        name: "John Willouby",
+        age: 28,
+        skillSet: "pipe fitting",
+        placeBorn: "New York, New York"
     },
     {
-      id: 5,
-      name: "Stan Honest",
-      age: 20,
-      skillSet: "boom-a-rang throwing",
-      placeBorn: "Perth, Australia"
+        id: 6,
+        name: "Stan Honest",
+        age: 20,
+        skillSet: "boom-a-rang throwing",
+        placeBorn: "Perth, Australia"
     },
     {
-      id: 6,
-      name: "Mia Watu",
-      age: 17,
-      skillSet: "acrobatics",
-      placeBorn: "Los Angeles, California"
+        id: 7,
+        name: "Mia Watu",
+        age: 17,
+        skillSet: "acrobatics",
+        placeBorn: "Los Angeles, California"
     },
     {
-      id: 7,
-      name: "Walter Cole",
-      age: 32,
-      skillSet: "jump rope",
-      placeBorn: "New Orleans, Louisiana"
+        id: 8,
+        name: "Walter Cole",
+        age: 32,
+        skillSet: "jump rope",
+        placeBorn: "New Orleans, Louisiana"
     },
-  ]
-  
-  const listOfPlayers = []
-  const blueTeam = []
-  const redTeam = []
-  
-  class Player {
-    constructor(id, name, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
-      this.id = id;
-      this.name = name;
-      this.canThrowBall = canThrowBall;
-      this.canDodgeBall = canDodgeBall; 
-      this.hasPaid = hasPaid;
-      this.isHealthy = isHealthy;
-      this.yearsExperience = yearsExperience;
+]
+
+const listOfPlayers = []
+const blueTeam = []
+const redTeam = []
+
+class player {
+    constructor(id, name, age, skillSet, placeBorn) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.skillSet = skillSet;
+        this.placeBorn = placeBorn;
+        this.canThrowBall;
+        this.canDodgeBall;
+        this.hasPaid;
+        this.isHealthy;
+        this.yearsExperience;
+        this.skillSetScore;
     }
-  }
-  class BlueTeammate {
-    constructor(id, name, teamColor, mascot){
-      this.id = id;
-      this.name = name;
-      this.teamColor = teamColor;
-      this.mascot = mascot;
+}
+class blueTeammate extends player {
+    constructor(id, name, age, skillSet, placeBorn) {
+        super(id, name, age, skillSet, placeBorn)
+        this.color = 'blue'
+        this.mascot = 'Elephant'
     }
-  }
-  class RedTeammate {
-    constructor(id, name, teamColor, mascot){
-      this.id = id;
-      this.name = name;
-      this.teamColor = teamColor;
-      this.mascot = mascot;
+}
+class redTeammate extends player {
+    constructor(id, name, age, skillSet, placeBorn) {
+        super(id, name, age, skillSet, placeBorn)
+        this.color = 'red'
+        this.mascot = 'Donkey'
     }
-  }
-  
-  const listPeopleChoices = () => {
-    const listElement = document.getElementById('people')
+}
+
+const listPeopleChoices = () => {
+
     arrOfPeople.map(person => {
-      const li = document.createElement("li")
-      const button = document.createElement("button")
-      button.innerHTML = "Make Player"
-      button.addEventListener('click', function() {
-        makePlayer(person.id) 
-        listElement.removeChild(li)
-      } )
-      li.appendChild(button)
-      li.appendChild(document.createTextNode(" " + person.name + " - " + person.skillSet))
-      listElement.append(li)
+        console.log(person.name + " - " + person.skillSet)
+
     })
-  }
-  
-  const makePlayer = (id) => {
-     let person = arrOfPeople.find(function (player) {
-     return player.id === id
+}
+
+
+const makePlayer = (id) => {
+    console.log(`li ${id} was clicked!`)
+}
+
+
+
+// Begin by reading the tests and building a function that will full each one.
+// As you build, you might not have to build them in order, maybe you do...
+// These are the tests
+if (typeof describe === 'function') {
+    describe('player', function () {
+        it('should have a the same paramters upon instantiation', function () {
+            // this creates a player with basic information an no skill sets
+            const newPlayer1 = new player(888, 'John Doe', 55, 'chemistry');
+            assert.equal(newPlayer1.id, 888);
+            assert.equal(newPlayer1.name, 'John Doe');
+            assert.equal(newPlayer1.age, 55);
+            assert.equal(newPlayer1.skillSet, 'chemistry');
+            assert.equal(newPlayer1.canDodgeBall, null);
+        });
+
+        it('can assign skills', function () {
+            // test that special skills can be assigned
+
+            const newPlayer1 = new player(888, 'John Doe', 55, 'chemistry');
+            newPlayer1.canThrowBall = '2'
+            newPlayer1.yearsExperience = '4'
+            assert.equal(newPlayer1.canThrowBall, '2');
+            assert.equal(newPlayer1.yearsExperience, '4');
+        });
     });
-    console.log(person)
-    let index = arrOfPeople.indexOf(person);
-    arrOfPeople.splice(index, 1);
-    const newPlayer = new Player (person.id, person.name, true, true, true, true, 5)
-    console.log(newPlayer)
-    listOfPlayers.push(newPlayer)
-    // console.log(listOfPlayers)
-    const listElement = document.getElementById('players')
-      const li = document.createElement("li")
-      redButton = document.createElement("button")
-      blueButton = document.createElement("button")
-      redButton.innerHTML = "Red"
-      redButton.style.color = "white"
-      redButton.style.backgroundColor = "tomato"
-      blueButton.innerHTML = "Blue"
-      blueButton.style.color = "white"
-      blueButton.style.backgroundColor = "royalBlue"
-      redButton.addEventListener('click', function() {
-        assignRedTeam(person.id)
-        listElement.removeChild(li)
-      })
-      blueButton.addEventListener('click', function() {
-        assignBlueTeam(person.id)
-        listElement.removeChild(li)
-      })
-      li.appendChild(redButton)
-      li.appendChild(blueButton)
-      li.appendChild(document.createTextNode(` ${person.name}`))
-      listElement.append(li)
-}; 
 
-const assignBlueTeam = (id) => {
-  let person = listOfPlayers.find(function (player) {
-    return player.id === id
-   });
-   let index = listOfPlayers.indexOf(person);
-   listOfPlayers.splice(index, 1);
-   const newBlueTeammate = new BlueTeammate (person.id, person.name, "Blue", "Ice Dragons")
-   blueTeam.push(newBlueTeammate)
-  const listElement = document.getElementById('blue')
-    const li = document.createElement("li")
-    undoButton = document.createElement("button")
-    undoButton.innerHTML = "Undo"
-    undoButton.addEventListener('click', function() {
-      undoBlue(person.id)
-      listElement.removeChild(li)
-    })
-    li.appendChild(undoButton)
-    li.appendChild(document.createTextNode(` ${person.name}, Blue Team, Ice Dragons`))
-    listElement.append(li)
-};
+    describe('player', function () {
+        it('should have a the same paramters upon instantiation', function () {
+            // this creates a blue player with basic information an shows
+            const newPlayer1 = new blueTeammate(888, 'John Doe', 55, 'chemistry');
+            assert.equal(newPlayer1.id, 888);
+            assert.equal(newPlayer1.name, 'John Doe');
+            assert.equal(newPlayer1.age, 55);
+            assert.equal(newPlayer1.skillSet, 'chemistry');
+            assert.equal(newPlayer1.canDodgeBall, null);
+        });
 
-const assignRedTeam = (id) => {
-  let person = listOfPlayers.find(function (player) {
-    return player.id === id
-  });
-  let index = listOfPlayers.indexOf(person);
-  listOfPlayers.splice(index, 1);
-  const newRedTeammate = new RedTeammate (person.id, person.name, "Red", "Sloths")
-  redTeam.push(newRedTeammate)
- const listElement = document.getElementById('red')
-   const li = document.createElement("li")
-   undoButton = document.createElement("button")
-   undoButton.innerHTML = "Undo"
-   undoButton.addEventListener('click', function() {
-      undoRed(person.id)
-     listElement.removeChild(li)
-   })
-   li.appendChild(undoButton)
-   li.appendChild(document.createTextNode(` ${person.name}, Red Team, Sloths`))
-   listElement.append(li)
-};
+        it('does assign color and mascot for blue', function () {
+            // shows that color and mascot are assigned
 
-const undoBlue = (id) => {
-  let person = blueTeam.find(function (player) {
-    return player.id === id
-   });
-   console.log(person)
-   let index = blueTeam.indexOf(person);
-   blueTeam.splice(index, 1);
-    const newPlayer = new Player (person.id, person.name, true, true, true, true, 5)
-   listOfPlayers.push(newPlayer)
-   const listElement = document.getElementById('players')
-     const li = document.createElement("li")
-     redButton = document.createElement("button")
-     blueButton = document.createElement("button")
-     redButton.innerHTML = "Red"
-     redButton.style.color = "white"
-     redButton.style.backgroundColor = "tomato"
-     blueButton.innerHTML = "Blue"
-     blueButton.style.color = "white"
-     blueButton.style.backgroundColor = "royalBlue"
-     redButton.addEventListener('click', function() {
-       assignRedTeam(person.id)
-       listElement.removeChild(li)
-     })
-     blueButton.addEventListener('click', function() {
-       assignBlueTeam(person.id)
-       listElement.removeChild(li)
-     })
-     li.appendChild(redButton)
-     li.appendChild(blueButton)
-     li.appendChild(document.createTextNode(" " + person.name))
-     listElement.append(li)
-}; 
-
-  const undoRed = (id) => {
-  let person = redTeam.find(function (player) {
-    return player.id === id
-   });
-   console.log(person)
-   let index = redTeam.indexOf(person);
-   redTeam.splice(index, 1);
-    const newPlayer = new Player (person.id, person.name, true, true, true, true, 5)
-   listOfPlayers.push(newPlayer)
-   const listElement = document.getElementById('players')
-     const li = document.createElement("li")
-     redButton = document.createElement("button")
-     blueButton = document.createElement("button")
-     redButton.innerHTML = "Red"
-     redButton.style.color = "white"
-     redButton.style.backgroundColor = "tomato"
-     blueButton.innerHTML = "Blue"
-     blueButton.style.color = "white"
-     blueButton.style.backgroundColor = "royalBlue"
-     redButton.addEventListener('click', function() {
-       assignRedTeam(person.id)
-       listElement.removeChild(li)
-     })
-     blueButton.addEventListener('click', function() {
-       assignBlueTeam(person.id)
-       listElement.removeChild(li)
-     })
-     li.appendChild(redButton)
-     li.appendChild(blueButton)
-     li.appendChild(document.createTextNode(" " + person.name))
-     listElement.append(li)
-}; 
-
-
-//Unit Tests
-if (typeof describe === 'function'){
-  describe('#makePlayer', () => {
-    it('can add a person from List of People to Dodgeball Players', function(){
-      // this adds a person from the list of players to the Dodgeball roster
-      const newPlayer = new Player(8, "Jane", true, false, true, true, 2);
-      // newPlayer.makePlayer(8);
-      assert.equal(newPlayer.name, "Jane");
-      assert.equal(newPlayer.yearsExperience, 2);
-      assert.equal(newPlayer.canDodgeBall, false);
+            const newPlayer1 = new blueTeammate(888, 'John Doe', 55, 'chemistry');
+            assert.equal(newPlayer1.color, 'blue');
+            assert.equal(newPlayer1.mascot, 'Elephant');
+        });
     });
-  });
-  describe('RedTeammate', function(){
-    it('should have an id, a name, a teamColor and mascot upon instantiation', function(){
-      // this creates a RedTeammate and passes the following arguments into its constructor:
-      // 'Amy Vasquez', 'red', 'Sloths'
-      const newRedTeammate = new RedTeammate(9, 'Amy Vasquez', 'red', 'Sloths');
-      assert.equal(newRedTeammate.id, 9);
-      assert.equal(newRedTeammate.name, 'Amy Vasquez');
-      assert.equal(newRedTeammate.teamColor, 'red');
-      assert.equal(newRedTeammate.mascot, 'Sloths');
+
+    describe('player', function () {
+        it('should have a the same paramters upon instantiation', function () {
+            // this creates a blue player with basic information an shows
+            const newPlayer1 = new blueTeammate(888, 'John Doe', 55, 'chemistry');
+            assert.equal(newPlayer1.id, 888);
+            assert.equal(newPlayer1.name, 'John Doe');
+            assert.equal(newPlayer1.age, 55);
+            assert.equal(newPlayer1.skillSet, 'chemistry');
+            assert.equal(newPlayer1.canDodgeBall, null);
+        });
+
+        it('does assign color and mascot for red', function () {
+            // shows that color and mascot are assigned
+
+            const newPlayer1 = new redTeammate(888, 'John Doe', 55, 'chemistry');
+            assert.equal(newPlayer1.color, 'red');
+            assert.equal(newPlayer1.mascot, 'Donkey');
+        });
     });
-  });
-  describe('#assignBlueTeam', () => {
-    it('can add a person from Dodgeball Players array to Blue Team', function(){
-      // this adds a person from the Dodgeball Players to Blue Team with mascot and TeamColor
-      const newBlueTeammate = new BlueTeammate(10, 'Marjorie', 'blue', 'Ice Dragons');
-      assert.equal(newBlueTeammate.id, 10);
-      assert.equal(newBlueTeammate.name, 'Marjorie');
-      assert.equal(newBlueTeammate.teamColor, 'blue');
-      assert.equal(newBlueTeammate.mascot, 'Ice Dragons');
-    });
-  });
 }
